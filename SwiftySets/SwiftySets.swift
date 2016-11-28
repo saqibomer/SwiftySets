8f8c332d13080d8936d61789ba20cfc11afa6979
Saqib Omer <saqibomer.cs@gmail.com>
2016-11-28 23:13:52 +0500

    Added Float, Character Strings Added Union functions for Int sets and Float Sets

--- SwiftySets/SwiftySets.swift
+++ SwiftySets/SwiftySets.swift
@@ -30,20 +30,21 @@ class SwiftySets {
      */
     var floatSet = Set<Float>()
     
-    /*
-     * A set which contains value of type String
-     */
-    var stringSet = Set<String>()
     
     /*
      * A set which contains value of type Characters
      */
-    var charSet = Set<Character>()
+    var alphaBetsSet = Set<Character>()
     
     
     /*
      * Create a set of type Int 
-     * This is a signed set. Set range can only be positive
+     * @params 
+     * startLimit -> Int
+     * endLimit   -> Int
+     * increement -> Int
+     *
+     * Retruns a set of int values
      */
     
     func createSignedIntSet(startLimit : Int , endLimit : Int, increement : Int) -> Set<Int>? {
@@ -65,7 +66,12 @@ class SwiftySets {
     
     /*
      * Create a set of type Int
-     * This is a signed set. Set range can only be positive
+     * @params
+     * startLimit -> Float
+     * endLimit   -> Float
+     * increement -> Float
+     *
+     * Retruns a set of float values
      */
     
     func createSignedFloatSet(startLimit : Float , endLimit : Float, increement : Float) -> Set<Float>? {
@@ -83,6 +89,88 @@ class SwiftySets {
         }
         return nil
         
+    }
+    
+    /*
+     * Create a set of type Alphabets
+     * @params
+     * startLimit -> Character
+     * endLimit   -> Character
+     * increement -> Character
+     * Takes either both small letters of both capital letters
+     * e.g startCharacter = "A" , endCharacter = "S"
+     * or startCharacter = "D" , endCharacter = "S"
+     *
+     * Retruns a set of alphabets
+     */
+    
+    func createAlphaBetsSet(startCharacter : Character , endCharacter : Character, increement : Int) -> Set<Character>? {
+        
+
+        
+        let scalarStartCharacter = String(startCharacter).unicodeScalars
+        let valStart             = scalarStartCharacter[scalarStartCharacter.startIndex].value
+        
+        let scalarEndCharacter   = String(endCharacter).unicodeScalars
+        let valEnd             = scalarEndCharacter[scalarEndCharacter.startIndex].value
+        
+        
+        if valStart >= 65 && valEnd <= 91 {
+            
+            for value in stride(from: valStart, through: valEnd, by: increement){
+                
+                alphaBetsSet.insert(Character(UnicodeScalar(value)!))
+                
+            }
+            
+            return alphaBetsSet
+            
+        }
+        
+        if valStart >= 97 && valEnd <= 122 {
+            
+            for value in stride(from: valStart, through: valEnd, by: increement){
+                
+                alphaBetsSet.insert(Character(UnicodeScalar(value)!))
+                
+            }
+            
+            return alphaBetsSet
+            
+        }
+        
+        
+        return nil
+        
+    }
+    
+    
+    /*
+     * Get Union of two Int sets
+     * @params
+     * firstSet -> Set
+     * secondSet   -> Set
+     
+     *
+     * Retruns a set of union of two sets
+     */
+    
+    func getUnionofIntSets(firstSet : Set<Int> , secondSet : Set<Int>)->Set<Int>? {
+        return firstSet.union(secondSet)
+    }
+    
+    /*
+     * Get Union of two Float sets
+     * @params
+     * firstSet -> Set
+     * secondSet   -> Set
+     
+     *
+     * Retruns a set of union of two sets
+     */
+    
+    func getUnionofFloatSets(firstSet : Set<Float> , secondSet : Set<Float>)->Set<Float>? {
+        return firstSet.union(secondSet)
     }
     
     
--- SwiftySets/ViewController.swift
+++ SwiftySets/ViewController.swift
@@ -18,15 +18,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        // Create a set of signed Int values set
-        if let anIntSet = sets.createSignedIntSet(startLimit: 5, endLimit: 20, increement: 5) {
-            print(anIntSet.sorted())
-        }
+            // Create a set of signed Int values set
+            if let anIntSet = sets.createSignedIntSet(startLimit: 5, endLimit: 20, increement: 5) {
+                print(anIntSet.sorted())
+            }
         
-        // Create a set of signed Float values set
-        if let aFloatSet = sets.createSignedFloatSet(startLimit: 5.0, endLimit: 20.0, increement : 2.0) {
-            print(aFloatSet.sorted())
-        }
+           // Create a set of signed Float values set
+            if let aFloatSet = sets.createSignedFloatSet(startLimit: 5.0, endLimit: 20.0, increement : 2.0) {
+                print(aFloatSet.sorted())
+            }
+    
+            // Create a set of signed Float values set
+            if let aAlphabetSet = sets.createAlphaBetsSet(startCharacter: "a", endCharacter: "z", increement: 1) {
+                print(aAlphabetSet.sorted())
+            }
+        
+            // Get Union of two Int Sets
+            let aSet = sets.createSignedIntSet(startLimit: 1, endLimit: 10, increement: 2)
+            let bSet = sets.createSignedIntSet(startLimit: 11, endLimit: 20, increement: 2)
+        
+            if let intUnion = sets.getUnionofIntSets(firstSet: aSet!, secondSet: bSet!) {
+                print(intUnion.sorted())
+            }
+        
+            // Get Union of two Int Sets
+            let floatOneSet = sets.createSignedFloatSet(startLimit: 2.0, endLimit: 8.0, increement: 1)
+            let floatTwoSet = sets.createSignedFloatSet(startLimit: 9.0, endLimit: 18.0, increement: 1)
+        
+            if let floatUnion = sets.getUnionofFloatSets(firstSet: floatOneSet!, secondSet: floatTwoSet!) {
+                print(floatUnion.sorted())
+            }
+    
         
         
         
